1f12171d854ef0356f8aa5d30ffbb3c923b4a521
Gaston <jgaston7@core.gtri.org>
2023-12-05 13:32:05 -0600

    adding solution to part 1

--- Day05/Day05.cpp
+++ Day05/Day05.cpp
@@ -54,7 +54,7 @@ void Day05::Configure(const ConfigurationResource configuration_resource,
     while (fgets(line, sizeof(line), fp) != NULL)
     {
       fgets(line, sizeof(line), fp);
-      while (line[0] != '\n')
+      while (line[0] != '\n' && !feof(fp))
       {
         sscanf(line, "%lu %lu %lu",
                &(m_mappings[m_num_mappings]
@@ -70,26 +70,22 @@ void Day05::Configure(const ConfigurationResource configuration_resource,
 
         fgets(line, sizeof(line), fp);
       }
-
-      m_num_mappings++;
-    }
-
-    for (unsigned int i = 0; i < m_num_seeds; i++)
-    {
-      printf("%lu\n", m_seeds[i]);
-    }
-    printf("\n");
-
-    for (unsigned int i = 0; i < m_num_mappings; i++)
-    {
-      for (unsigned int j = 0; j < m_mappings[i].num_ranges; j++)
+      if (feof(fp))
       {
-        printf("%lu %lu %lu\n", m_mappings[i].ranges[j].destination_start,
-               m_mappings[i].ranges[j].source_start,
-               m_mappings[i].ranges[j].range);
+        sscanf(line, "%lu %lu %lu",
+               &(m_mappings[m_num_mappings]
+                   .ranges[m_mappings[m_num_mappings].num_ranges]
+                   .destination_start),
+               &(m_mappings[m_num_mappings]
+                   .ranges[m_mappings[m_num_mappings].num_ranges]
+                   .source_start),
+               &(m_mappings[m_num_mappings]
+                   .ranges[m_mappings[m_num_mappings].num_ranges]
+                   .range));
+        m_mappings[m_num_mappings].num_ranges++;
       }
 
-      printf("\n");
+      m_num_mappings++;
     }
   }
 }
@@ -97,6 +93,35 @@ void Day05::Configure(const ConfigurationResource configuration_resource,
 void Day05::Solve(RETURN_CODE_TYPE::Value& return_code)
 {
   return_code = RETURN_CODE_TYPE::NO_ERROR;
+
+  for (unsigned int i = 0; i < m_num_seeds; i++)
+  {
+    for (unsigned int j = 0; j < m_num_mappings; j++)
+    {
+      bool mapping_found = false;
+      for (unsigned int k = 0; k < m_mappings[j].num_ranges && !mapping_found;
+           k++)
+      {
+        if (m_mappings[j].ranges[k].source_start <= m_seeds[i] &&
+            m_seeds[i] <= m_mappings[j].ranges[k].source_start +
+                            m_mappings[j].ranges[k].range)
+        {
+          m_seeds[i] += m_mappings[j].ranges[k].destination_start -
+                        m_mappings[j].ranges[k].source_start;
+          mapping_found = true;
+        }
+      }
+    }
+  }
+
+  unsigned int lowest_location_number = 0xFFFFFFFF;
+  for (unsigned int i = 0; i < m_num_seeds; i++)
+  {
+    if (m_seeds[i] < lowest_location_number)
+      lowest_location_number = m_seeds[i];
+  }
+
+  printf("Part 1 solution: %lu\n", lowest_location_number);
 }
 
 void Day05::Finalize(RETURN_CODE_TYPE::Value& return_code)

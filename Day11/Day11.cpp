efcf68b68383af89c312e7d19aa5db94dd1cb626
Gaston <jgaston7@core.gtri.org>
2023-12-11 12:11:50 -0600

    completed file parsing

--- Day11/Day11.cpp
+++ Day11/Day11.cpp
@@ -5,7 +5,7 @@
 
 using namespace DAY11;
 
-Day11::Day11() {}
+Day11::Day11() : m_num_rows(0), m_num_cols(0), m_num_galaxies(0) {}
 
 Day11::~Day11()
 {
@@ -33,6 +33,81 @@ void Day11::Configure(const ConfigurationResource configuration_resource,
   {
     char line[512] = {0};
 
+    bool num_cols_initialized = false;
+
+    // Iterate over each row
+    while (fgets(line, sizeof(line), fp) != NULL &&
+           return_code == RETURN_CODE_TYPE::NO_ERROR)
+    {
+
+      // On first pass find number of columns
+      if (!num_cols_initialized)
+      {
+        for (uint32_t i = 0;
+             line[i] != '\n' && return_code == RETURN_CODE_TYPE::NO_ERROR; i++)
+        {
+          m_num_cols++;
+          if (m_num_cols > MAX_NUM_COLS)
+            return_code = RETURN_CODE_TYPE::MEMORY_EXCEEDED;
+        }
+        num_cols_initialized = true;
+      }
+
+      // Iterate over each column to determine type
+      for (uint32_t i = 0;
+           i < m_num_cols && return_code == RETURN_CODE_TYPE::NO_ERROR; i++)
+      {
+        Tile& tile = m_space[m_num_rows][i];
+        switch (line[i])
+        {
+        case '.':
+          tile.type = Space;
+          break;
+        case '#':
+          tile.type = Galaxy;
+          tile.id   = m_num_galaxies++; // Assign id to each galaxy
+          break;
+        }
+      }
+
+      m_num_rows++;
+      if (m_num_rows > MAX_NUM_ROWS)
+        return_code = RETURN_CODE_TYPE::MEMORY_EXCEEDED;
+    }
+
+    // Create memory to store galaxy cooridnates
+    m_galaxy_coordinate_list = new Position[m_num_galaxies];
+
+    // Keep track of number of galaxies in each row and column
+    // to determine if expansion has occured
+    uint32_t row_galaxy_count[MAX_NUM_ROWS] = {0};
+    uint32_t col_galaxy_count[MAX_NUM_COLS] = {0};
+    for (uint32_t i = 0; i < m_num_rows; i++)
+    {
+      for (uint32_t j = 0; j < m_num_cols; j++)
+      {
+        if (m_space[i][j].type == Galaxy)
+        {
+          // Increment galaxy count
+          row_galaxy_count[i]++;
+          col_galaxy_count[j]++;
+          // Add coordinates to coordinate list
+          m_galaxy_coordinate_list[i].x = i;
+          m_galaxy_coordinate_list[i].y = j;
+        }
+      }
+    }
+
+    // Update distances based on empty space found
+    for (uint32_t i = 0; i < m_num_rows; i++)
+    {
+      for (uint32_t j = 0; j < m_num_cols; j++)
+      {
+        if (row_galaxy_count[i] == 0 || col_galaxy_count[j] == 0)
+          m_space[i][j].distance *= 2;
+      }
+    }
+
     fclose(fp);
   }
 }
@@ -40,6 +115,15 @@ void Day11::Configure(const ConfigurationResource configuration_resource,
 void Day11::Solve(RETURN_CODE_TYPE::Value& return_code)
 {
   return_code = RETURN_CODE_TYPE::NO_ERROR;
+
+  for (uint32_t i = 0; i < m_num_galaxies - 1; i++)
+  {
+    for (uint32_t j = i + 1; j < m_num_galaxies; j++)
+    {
+      Position start_position = m_galaxy_coordinate_list[i];
+      Position end_position   = m_galaxy_coordinate_list[j];
+    }
+  }
 }
 
 void Day11::Finalize(RETURN_CODE_TYPE::Value& return_code)
--- Day11/Day11.hpp
+++ Day11/Day11.hpp
@@ -6,6 +6,29 @@
 namespace DAY11
 {
 
+const uint32_t MAX_NUM_ROWS = 140;
+const uint32_t MAX_NUM_COLS = 140;
+
+enum TileType
+{
+  Space,
+  Galaxy,
+};
+
+struct Tile
+{
+  uint32_t id       = 0;
+  TileType type     = Space;
+  uint32_t distance = 1;
+  bool visited      = false;
+};
+
+struct Position
+{
+  uint32_t x = 0;
+  uint32_t y = 0;
+};
+
 class Day11 : public Solver
 {
   public:
@@ -18,6 +41,11 @@ class Day11 : public Solver
   void Finalize(RETURN_CODE_TYPE::Value& return_code);
 
   private:
+  uint32_t m_num_rows;
+  uint32_t m_num_cols;
+  Tile m_space[MAX_NUM_ROWS][MAX_NUM_COLS];
+  uint32_t m_num_galaxies;
+  Position* m_galaxy_coordinate_list;
 };
 
 } // namespace DAY11

59e621c95b6891f0ba4061c6ee9d31c5c4420d8a
Gaston <jgaston7@core.gtri.org>
2023-12-12 17:04:21 -0600

    adding start of Day 12

--- Day12/Day12.cpp
+++ Day12/Day12.cpp
@@ -1,11 +1,12 @@
 #include <cstdio>
 #include <cstring>
+#include <math.h>
 
 #include "Day12.hpp"
 
 using namespace DAY12;
 
-Day12::Day12() {}
+Day12::Day12() : m_num_records(0) {}
 
 Day12::~Day12()
 {
@@ -16,6 +17,7 @@ Day12::~Day12()
 void Day12::Initialize(RETURN_CODE_TYPE::Value& return_code)
 {
   return_code = RETURN_CODE_TYPE::NO_ERROR;
+  memset(m_records, 0, sizeof(m_records));
 }
 
 void Day12::Configure(const ConfigurationResource configuration_resource,
@@ -33,6 +35,22 @@ void Day12::Configure(const ConfigurationResource configuration_resource,
   {
     char line[512] = {0};
 
+    while (fgets(line, sizeof(line), fp) != NULL)
+    {
+      Record& record = m_records[m_num_records++];
+
+      char* line_ptr = &(line[0]);
+      line_ptr       = strtok(line_ptr, " ");
+      strcpy(record.spring_conditions, line_ptr);
+
+      line_ptr = strtok(NULL, ",");
+      while (line_ptr != NULL)
+      {
+        sscanf(line_ptr, "%lu", &(record.sets[record.num_sets++]));
+        line_ptr = strtok(NULL, ",");
+      }
+    }
+
     fclose(fp);
   }
 }
@@ -40,6 +58,30 @@ void Day12::Configure(const ConfigurationResource configuration_resource,
 void Day12::Solve(RETURN_CODE_TYPE::Value& return_code)
 {
   return_code = RETURN_CODE_TYPE::NO_ERROR;
+
+  for (uint32_t i = 0; i < m_num_records; i++)
+  {
+    Record& record = m_records[i];
+
+    uint32_t num_springs = strlen(record.spring_conditions);
+    uint32_t num_unknown = 0;
+    for (uint32_t j = 0; j < num_springs; j++)
+    {
+      if (record.spring_conditions[j] == '?')
+        num_unknown++;
+    }
+
+    uint32_t num_combinations = pow(2, num_unknown);
+
+    for (uint32_t j = 0; j < num_combinations; j++)
+    {
+      char potential_record[32];
+      for (uint32_t k = 0; k < num_unknown; k++)
+      {
+        bool is_operational = (j >> ((num_unknown - 1) - k)) & 0x01;
+      }
+    }
+  }
 }
 
 void Day12::Finalize(RETURN_CODE_TYPE::Value& return_code)
--- Day12/Day12.hpp
+++ Day12/Day12.hpp
@@ -6,6 +6,15 @@
 namespace DAY12
 {
 
+const uint32_t MAX_NUM_RECORDS = 1000;
+
+struct Record
+{
+  char spring_conditions[32];
+  uint32_t num_sets = 0;
+  uint32_t sets[16];
+};
+
 class Day12 : public Solver
 {
   public:
@@ -18,6 +27,8 @@ class Day12 : public Solver
   void Finalize(RETURN_CODE_TYPE::Value& return_code);
 
   private:
+  uint32_t m_num_records;
+  Record m_records[MAX_NUM_RECORDS];
 };
 
 } // namespace DAY12

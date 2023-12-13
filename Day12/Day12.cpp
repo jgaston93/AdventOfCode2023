242eaf050a47e1e739a22ac878a21512b3c7cfbc
Gaston <jgaston7@core.gtri.org>
2023-12-13 07:45:23 -0600

    completed day 12 part 1

--- Day12/Day12.cpp
+++ Day12/Day12.cpp
@@ -59,29 +59,82 @@ void Day12::Solve(RETURN_CODE_TYPE::Value& return_code)
 {
   return_code = RETURN_CODE_TYPE::NO_ERROR;
 
+  uint32_t num_valid_permutations_sum = 0;
+
   for (uint32_t i = 0; i < m_num_records; i++)
   {
     Record& record = m_records[i];
 
     uint32_t num_springs = strlen(record.spring_conditions);
-    uint32_t num_unknown = 0;
+
+    // Count number of unknown conditions and track indices
+    uint32_t num_unknown         = 0;
+    uint32_t unknown_indices[32] = {0};
     for (uint32_t j = 0; j < num_springs; j++)
     {
       if (record.spring_conditions[j] == '?')
-        num_unknown++;
+        unknown_indices[num_unknown++] = j;
     }
 
-    uint32_t num_combinations = pow(2, num_unknown);
+    // Get number of permutations based number of unknown conditions
+    uint32_t num_permutations = pow(2, num_unknown);
 
-    for (uint32_t j = 0; j < num_combinations; j++)
+    uint32_t num_valid_permutations = 0;
+
+    // Check each permutation
+    for (uint32_t j = 0; j < num_permutations; j++)
     {
-      char potential_record[32];
+      // Fill potential record with current conditions
+      char potential_record[32] = {0};
+      for (uint32_t k = 0; k < num_springs; k++)
+        potential_record[k] = record.spring_conditions[k];
+
+      // Fill unknowns based on current permutation
       for (uint32_t k = 0; k < num_unknown; k++)
       {
         bool is_operational = (j >> ((num_unknown - 1) - k)) & 0x01;
+        if (is_operational)
+          potential_record[unknown_indices[k]] = '.';
+        else
+          potential_record[unknown_indices[k]] = '#';
       }
+
+      // Check if record is valid
+      uint32_t potential_sets[16] = {0};
+      uint32_t num_potential_sets = 0;
+
+      // Find number and count of contiguous sets
+      char* cond_ptr = &(potential_record[0]);
+      cond_ptr       = strtok(cond_ptr, ".");
+      while (cond_ptr != NULL)
+      {
+        uint32_t len = strlen(cond_ptr);
+        for (uint32_t k = 0; k < len; k++)
+          potential_sets[num_potential_sets]++;
+        num_potential_sets++;
+        cond_ptr = strtok(NULL, ".");
+      }
+
+      // Check if number of sets match the valid amount
+      bool permutation_valid = true;
+      if (num_potential_sets != record.num_sets)
+      {
+        permutation_valid = false;
+      }
+
+      for (uint32_t k = 0; k < num_potential_sets && permutation_valid; k++)
+      {
+        if (potential_sets[k] != record.sets[k])
+          permutation_valid = false;
+      }
+
+      if (permutation_valid)
+        num_valid_permutations++;
     }
+    num_valid_permutations_sum += num_valid_permutations;
   }
+
+  printf("Part 1 solution: %lu\n", num_valid_permutations_sum);
 }
 
 void Day12::Finalize(RETURN_CODE_TYPE::Value& return_code)

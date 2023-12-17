07fdf4e94e028bcc84d24ddefda45e81ed974ceb
Gaston <jgaston7@core.gtri.org>
2023-12-17 09:07:01 -0600

    big ugly part 2 solution

--- Day16/Day16.cpp
+++ Day16/Day16.cpp
@@ -5,7 +5,7 @@
 
 using namespace DAY16;
 
-Day16::Day16() : m_grid_size(0), m_num_beams(0), m_beam_id(0) {}
+Day16::Day16() : m_grid_size(0), m_num_beams(0) {}
 
 Day16::~Day16()
 {
@@ -74,84 +74,242 @@ void Day16::Solve(RETURN_CODE_TYPE::Value& return_code)
 {
   return_code = RETURN_CODE_TYPE::NO_ERROR;
 
-  m_beams[0].beam_id = m_beam_id++;
-  m_beams[0].vel_x   = 1;
-  m_num_beams        = 1;
+  uint32_t max_energy_sum = 0;
 
-  Interact(m_beams[0], m_tiles[m_beams[0].y][m_beams[0].x]);
-
-  bool running          = true;
-  uint32_t step_counter = 0;
-
-  while (running)
+  for (uint32_t y = 0; y < m_grid_size; y++)
   {
-    uint32_t current_num_beams = m_num_beams;
-    for (uint32_t i = 0; i < current_num_beams; i++)
+    for (uint32_t x = 0; x < m_grid_size; x++)
     {
-      Beam& beam = m_beams[i];
-
-      beam.x += beam.vel_x;
-      beam.y += beam.vel_y;
-
-      if (beam.x < 0)
+      if (y == 0 && x == 0)
       {
-        beam.x     = 0;
-        beam.vel_x = 0;
-      }
-      else if (beam.x >= m_grid_size)
-      {
-        beam.x     = m_grid_size - 1;
-        beam.vel_x = 0;
-      }
+        // Seed initial beam
+        m_beams[0].x     = x;
+        m_beams[0].y     = y;
+        m_beams[0].vel_x = 1;
+        m_beams[0].vel_y = 0;
+        m_num_beams      = 1;
 
-      if (beam.y < 0)
-      {
-        beam.y     = 0;
-        beam.vel_y = 0;
-      }
-      else if (beam.y >= m_grid_size)
-      {
-        beam.y     = m_grid_size - 1;
-        beam.vel_y = 0;
-      }
+        // Run beam simulation
+        RunBeamSimulation();
 
-      Interact(beam, m_tiles[beam.y][beam.x]);
+        // Calculate final energy level sum
+        uint32_t energy_sum = CalculateEnergySum();
+        if (energy_sum > max_energy_sum)
+          max_energy_sum = energy_sum;
+
+        printf("Part 1 solution: %lu\n", energy_sum);
+
+        ResetEnergyLevels();
+
+        // Seed initial beam
+        m_beams[0].x     = x;
+        m_beams[0].y     = y;
+        m_beams[0].vel_x = 0;
+        m_beams[0].vel_y = 1;
+        m_num_beams      = 1;
+
+        // Run beam simulation
+        RunBeamSimulation();
+
+        // Calculate final energy level sum
+        energy_sum = CalculateEnergySum();
+        if (energy_sum > max_energy_sum)
+          max_energy_sum = energy_sum;
+
+        // Reset energy levels for new simulation
+        ResetEnergyLevels();
+      }
+      else if (y == 0 && x == m_grid_size - 1)
+      {
+        // Seed initial beam
+        m_beams[0].x     = x;
+        m_beams[0].y     = y;
+        m_beams[0].vel_x = -1;
+        m_beams[0].vel_y = 0;
+        m_num_beams      = 1;
+
+        // Run beam simulation
+        RunBeamSimulation();
+
+        // Calculate final energy level sum
+        uint32_t energy_sum = CalculateEnergySum();
+        if (energy_sum > max_energy_sum)
+          max_energy_sum = energy_sum;
+
+        ResetEnergyLevels();
+
+        // Seed initial beam
+        m_beams[0].x     = x;
+        m_beams[0].y     = y;
+        m_beams[0].vel_x = 0;
+        m_beams[0].vel_y = 1;
+        m_num_beams      = 1;
+
+        // Run beam simulation
+        RunBeamSimulation();
+
+        // Calculate final energy level sum
+        energy_sum = CalculateEnergySum();
+        if (energy_sum > max_energy_sum)
+          max_energy_sum = energy_sum;
+
+        // Reset energy levels for new simulation
+        ResetEnergyLevels();
+      }
+      else if (y == m_grid_size - 1 && x == 0)
+      {
+        // Seed initial beam
+        m_beams[0].x     = x;
+        m_beams[0].y     = y;
+        m_beams[0].vel_x = 1;
+        m_beams[0].vel_y = 0;
+        m_num_beams      = 1;
+
+        // Run beam simulation
+        RunBeamSimulation();
+
+        // Calculate final energy level sum
+        uint32_t energy_sum = CalculateEnergySum();
+        if (energy_sum > max_energy_sum)
+          max_energy_sum = energy_sum;
+
+        ResetEnergyLevels();
+
+        // Seed initial beam
+        m_beams[0].x     = x;
+        m_beams[0].y     = y;
+        m_beams[0].vel_x = 0;
+        m_beams[0].vel_y = -1;
+        m_num_beams      = 1;
+
+        // Run beam simulation
+        RunBeamSimulation();
+
+        // Calculate final energy level sum
+        energy_sum = CalculateEnergySum();
+        if (energy_sum > max_energy_sum)
+          max_energy_sum = energy_sum;
+
+        // Reset energy levels for new simulation
+        ResetEnergyLevels();
+      }
+      else if (y == m_grid_size - 1 && x == m_grid_size - 1)
+      {
+        // Seed initial beam
+        m_beams[0].x     = x;
+        m_beams[0].y     = y;
+        m_beams[0].vel_x = -1;
+        m_beams[0].vel_y = 0;
+        m_num_beams      = 1;
+
+        // Run beam simulation
+        RunBeamSimulation();
+
+        // Calculate final energy level sum
+        uint32_t energy_sum = CalculateEnergySum();
+        if (energy_sum > max_energy_sum)
+          max_energy_sum = energy_sum;
+
+        ResetEnergyLevels();
+
+        // Seed initial beam
+        m_beams[0].x     = x;
+        m_beams[0].y     = y;
+        m_beams[0].vel_x = 0;
+        m_beams[0].vel_y = -1;
+        m_num_beams      = 1;
+
+        // Run beam simulation
+        RunBeamSimulation();
+
+        // Calculate final energy level sum
+        energy_sum = CalculateEnergySum();
+        if (energy_sum > max_energy_sum)
+          max_energy_sum = energy_sum;
+
+        // Reset energy levels for new simulation
+        ResetEnergyLevels();
+      }
+      else if (y == 0)
+      {
+        // Seed initial beam
+        m_beams[0].x     = x;
+        m_beams[0].y     = y;
+        m_beams[0].vel_x = 0;
+        m_beams[0].vel_y = 1;
+        m_num_beams      = 1;
+
+        // Run beam simulation
+        RunBeamSimulation();
+
+        // Calculate final energy level sum
+        uint32_t energy_sum = CalculateEnergySum();
+        if (energy_sum > max_energy_sum)
+          max_energy_sum = energy_sum;
+
+        ResetEnergyLevels();
+      }
+      else if (y == m_grid_size - 1)
+      {
+        // Seed initial beam
+        m_beams[0].x     = x;
+        m_beams[0].y     = y;
+        m_beams[0].vel_x = 0;
+        m_beams[0].vel_y = -1;
+        m_num_beams      = 1;
+
+        // Run beam simulation
+        RunBeamSimulation();
+
+        // Calculate final energy level sum
+        uint32_t energy_sum = CalculateEnergySum();
+        if (energy_sum > max_energy_sum)
+          max_energy_sum = energy_sum;
+
+        ResetEnergyLevels();
+      }
+      else if (x == 0)
+      {
+        // Seed initial beam
+        m_beams[0].x     = x;
+        m_beams[0].y     = y;
+        m_beams[0].vel_x = 1;
+        m_beams[0].vel_y = 0;
+        m_num_beams      = 1;
+
+        // Run beam simulation
+        RunBeamSimulation();
+
+        // Calculate final energy level sum
+        uint32_t energy_sum = CalculateEnergySum();
+        if (energy_sum > max_energy_sum)
+          max_energy_sum = energy_sum;
+
+        ResetEnergyLevels();
+      }
+      else if (x == m_grid_size - 1)
+      {
+        // Seed initial beam
+        m_beams[0].x     = x;
+        m_beams[0].y     = y;
+        m_beams[0].vel_x = -1;
+        m_beams[0].vel_y = 0;
+        m_num_beams      = 1;
+
+        // Run beam simulation
+        RunBeamSimulation();
+
+        // Calculate final energy level sum
+        uint32_t energy_sum = CalculateEnergySum();
+        if (energy_sum > max_energy_sum)
+          max_energy_sum = energy_sum;
+
+        ResetEnergyLevels();
+      }
     }
-
-    for (uint32_t i = 0; i < m_num_beams; i++)
-    {
-      if (m_beams[i].vel_x == 0 && m_beams[i].vel_y == 0)
-      {
-        for (uint32_t j = i; j < m_num_beams - 1; j++)
-        {
-          Beam swap      = m_beams[j];
-          m_beams[j]     = m_beams[j + 1];
-          m_beams[j + 1] = swap;
-        }
-        m_num_beams--;
-      }
-    }
-    if (step_counter++ > 2000)
-      running = false;
   }
 
-  uint32_t energy_sum = 0;
-  for (uint32_t i = 0; i < m_grid_size; i++)
-  {
-    for (uint32_t j = 0; j < m_grid_size; j++)
-    {
-      if (m_tiles[i][j].energy_level > 0)
-      {
-        energy_sum++;
-        printf("#");
-      }
-      else
-        printf(".");
-    }
-    printf("\n");
-  }
-  printf("\n");
-  printf("Part 1 solution: %lu\n", energy_sum);
+  printf("Part 2 solution: %lu\n", max_energy_sum);
 }
 
 void Day16::Finalize(RETURN_CODE_TYPE::Value& return_code)
@@ -212,162 +370,139 @@ void Day16::Interact(Beam& beam, Tile& tile)
   }
   else if (tile.type == VerticalSplitter)
   {
-    bool beam_found = false;
-    for (uint32_t i = 0; i < tile.num_visited_beams && !beam_found; i++)
-    {
-      if (beam.beam_id == tile.visited_beams[i])
-        beam_found = true;
-    }
 
-    if (beam_found && beam.vel_x != 0)
+    if (tile.energy_level > 2)
     {
       beam.vel_x = 0;
       beam.vel_y = 0;
     }
-    else
+    else if (beam.vel_x != 0)
     {
-      if (beam.vel_x > 0)
+      beam.vel_x = 0;
+      beam.vel_y = 1;
+
+      if (m_num_beams < MAX_NUM_BEAMS)
       {
-        beam.vel_x = 0;
-        beam.vel_y = -1;
-
-        if (tile.num_visited_beams < MAX_NUM_BEAMS)
-          tile.visited_beams[tile.num_visited_beams++] = beam.beam_id;
-        else
-          printf("Too many visited beams\n");
-
-        if (m_num_beams < MAX_NUM_BEAMS)
-        {
-          m_beams[m_num_beams].beam_id = m_beam_id++;
-          m_beams[m_num_beams].x       = beam.x;
-          m_beams[m_num_beams].y       = beam.y;
-          m_beams[m_num_beams].vel_x   = 0;
-          m_beams[m_num_beams].vel_y   = 1;
-
-          if (tile.num_visited_beams < MAX_NUM_BEAMS)
-            tile.visited_beams[tile.num_visited_beams++] =
-              m_beams[m_num_beams].beam_id;
-          else
-            printf("Too many visited beams\n");
-
-          m_num_beams++;
-        }
-        else
-        {
-          // printf("Warning too many beams created\n");
-        }
+        m_beams[m_num_beams].x     = beam.x;
+        m_beams[m_num_beams].y     = beam.y;
+        m_beams[m_num_beams].vel_x = 0;
+        m_beams[m_num_beams].vel_y = -1;
+        m_num_beams++;
       }
-      else if (beam.vel_x < 0)
+      else
       {
+        printf("Warning too many beams created\n");
         beam.vel_x = 0;
-        beam.vel_y = 1;
-
-        if (tile.num_visited_beams < MAX_NUM_BEAMS)
-          tile.visited_beams[tile.num_visited_beams++] = beam.beam_id;
-        else
-          printf("Too many visited beams\n");
-
-        if (m_num_beams < MAX_NUM_BEAMS)
-        {
-          m_beams[m_num_beams].beam_id = m_beam_id++;
-          m_beams[m_num_beams].x       = beam.x;
-          m_beams[m_num_beams].y       = beam.y;
-          m_beams[m_num_beams].vel_x   = 0;
-          m_beams[m_num_beams].vel_y   = -1;
-
-          if (tile.num_visited_beams < MAX_NUM_BEAMS)
-            tile.visited_beams[tile.num_visited_beams++] =
-              m_beams[m_num_beams].beam_id;
-          else
-            printf("Too many visited beams\n");
-
-          m_num_beams++;
-        }
-        else
-        {
-          // printf("Warning too many beams created\n");
-        }
+        beam.vel_y = 0;
       }
     }
   }
   else if (tile.type == HorizontalSplitter)
   {
-    bool beam_found = false;
-    for (uint32_t i = 0; i < tile.num_visited_beams && !beam_found; i++)
-    {
-      if (beam.beam_id == tile.visited_beams[i])
-        beam_found = true;
-    }
-
-    if (beam_found && beam.vel_y != 0)
+    if (tile.energy_level > 2)
     {
       beam.vel_x = 0;
       beam.vel_y = 0;
     }
-    else
+    else if (beam.vel_y != 0)
     {
-      if (beam.vel_y > 0)
+      beam.vel_y = 0;
+      beam.vel_x = 1;
+
+      if (m_num_beams < MAX_NUM_BEAMS)
       {
-        beam.vel_y = 0;
-        beam.vel_x = -1;
-
-        if (tile.num_visited_beams < MAX_NUM_BEAMS)
-          tile.visited_beams[tile.num_visited_beams++] = beam.beam_id;
-        else
-          printf("Too many visited beams\n");
-
-        if (m_num_beams < MAX_NUM_BEAMS)
-        {
-          m_beams[m_num_beams].beam_id = m_beam_id++;
-          m_beams[m_num_beams].x       = beam.x;
-          m_beams[m_num_beams].y       = beam.y;
-          m_beams[m_num_beams].vel_y   = 0;
-          m_beams[m_num_beams].vel_x   = 1;
-
-          if (tile.num_visited_beams < MAX_NUM_BEAMS)
-            tile.visited_beams[tile.num_visited_beams++] =
-              m_beams[m_num_beams].beam_id;
-          else
-            printf("Too many visited beams\n");
-
-          m_num_beams++;
-        }
-        else
-        {
-          // printf("Warning too many beams created\n");
-        }
+        m_beams[m_num_beams].x     = beam.x;
+        m_beams[m_num_beams].y     = beam.y;
+        m_beams[m_num_beams].vel_y = 0;
+        m_beams[m_num_beams].vel_x = -1;
+        m_num_beams++;
       }
-      else if (beam.vel_y < 0)
+      else
       {
+        printf("Warning too many beams created\n");
+        beam.vel_x = 0;
         beam.vel_y = 0;
-        beam.vel_x = 1;
-
-        if (tile.num_visited_beams < MAX_NUM_BEAMS)
-          tile.visited_beams[tile.num_visited_beams++] = beam.beam_id;
-        else
-          printf("Too many visited beams\n");
-
-        if (m_num_beams < MAX_NUM_BEAMS)
-        {
-          m_beams[m_num_beams].beam_id = m_beam_id++;
-          m_beams[m_num_beams].x       = beam.x;
-          m_beams[m_num_beams].y       = beam.y;
-          m_beams[m_num_beams].vel_y   = 0;
-          m_beams[m_num_beams].vel_x   = -1;
-
-          if (tile.num_visited_beams < MAX_NUM_BEAMS)
-            tile.visited_beams[tile.num_visited_beams++] =
-              m_beams[m_num_beams].beam_id;
-          else
-            printf("Too many visited beams\n");
-
-          m_num_beams++;
-        }
-        else
-        {
-          // printf("Warning too many beams created\n");
-        }
       }
     }
+  }
+}
+
+void Day16::ResetEnergyLevels()
+{
+
+  for (uint32_t i = 0; i < m_grid_size; i++)
+  {
+    for (uint32_t j = 0; j < m_grid_size; j++)
+    {
+      m_tiles[i][j].energy_level = 0;
+    }
+  }
+}
+
+uint32_t Day16::CalculateEnergySum()
+{
+  uint32_t energy_sum = 0;
+  for (uint32_t i = 0; i < m_grid_size; i++)
+  {
+    for (uint32_t j = 0; j < m_grid_size; j++)
+    {
+      if (m_tiles[i][j].energy_level > 0)
+      {
+        energy_sum++;
+      }
+    }
+  }
+
+  return energy_sum;
+}
+
+void Day16::RunBeamSimulation()
+{
+  Interact(m_beams[0], m_tiles[m_beams[0].y][m_beams[0].x]);
+
+  bool running          = true;
+  uint32_t step_counter = 0;
+
+  while (running)
+  {
+    uint32_t current_num_beams = m_num_beams;
+    uint32_t num_moving_beams  = 0;
+    for (uint32_t i = 0; i < current_num_beams; i++)
+    {
+      Beam& beam = m_beams[i];
+      if (beam.vel_x != 0 || beam.vel_y != 0)
+      {
+        num_moving_beams++;
+        beam.x += beam.vel_x;
+        beam.y += beam.vel_y;
+
+        if (beam.x < 0)
+        {
+          beam.x     = 0;
+          beam.vel_x = 0;
+        }
+        else if (beam.x >= m_grid_size)
+        {
+          beam.x     = m_grid_size - 1;
+          beam.vel_x = 0;
+        }
+
+        if (beam.y < 0)
+        {
+          beam.y     = 0;
+          beam.vel_y = 0;
+        }
+        else if (beam.y >= m_grid_size)
+        {
+          beam.y     = m_grid_size - 1;
+          beam.vel_y = 0;
+        }
+
+        Interact(beam, m_tiles[beam.y][beam.x]);
+      }
+    }
+
+    if (num_moving_beams == 0)
+      running = false;
   }
 }
--- Day16/Day16.hpp
+++ Day16/Day16.hpp
@@ -7,7 +7,7 @@ namespace DAY16
 {
 
 const uint32_t MAX_GRID_SIZE = 110;
-const uint32_t MAX_NUM_BEAMS = 8192;
+const uint32_t MAX_NUM_BEAMS = 2048;
 
 enum TileType
 {
@@ -20,19 +20,16 @@ enum TileType
 
 struct Tile
 {
-  TileType type              = Empty;
-  uint32_t energy_level      = 0;
-  uint64_t num_visited_beams = 0;
-  uint64_t visited_beams[MAX_NUM_BEAMS];
+  TileType type         = Empty;
+  uint32_t energy_level = 0;
 };
 
 struct Beam
 {
-  uint64_t beam_id = 0;
-  int32_t x        = 0;
-  int32_t y        = 0;
-  int32_t vel_x    = 0;
-  int32_t vel_y    = 0;
+  int32_t x     = 0;
+  int32_t y     = 0;
+  int32_t vel_x = 0;
+  int32_t vel_y = 0;
 };
 
 class Day16 : public Solver
@@ -46,12 +43,15 @@ class Day16 : public Solver
   void Solve(RETURN_CODE_TYPE::Value& return_code);
   void Finalize(RETURN_CODE_TYPE::Value& return_code);
 
+  void ResetEnergyLevels();
+  uint32_t CalculateEnergySum();
+  void RunBeamSimulation();
+
   private:
   uint32_t m_grid_size;
   Tile m_tiles[MAX_GRID_SIZE][MAX_GRID_SIZE];
   uint32_t m_num_beams;
   Beam m_beams[MAX_NUM_BEAMS];
-  uint64_t m_beam_id;
 
   void Interact(Beam& beam, Tile& tile);
 };
